bfeb6c781343385724b1c63835df1b581ffee84a
Jen Jayme <jeniferjayme@gmail.com>
2020-07-14 15:03:26 -0700

    Added activities and solutions from today's class.

--- 10-OOP/activities/09-Stu_TDD/Unsolved/test/algo.test.js
+++ 10-OOP/activities/09-Stu_TDD/Unsolved/test/algo.test.js
@@ -2,14 +2,23 @@ const Algo = require("../algo");
 
 describe("Algo", () => {
   describe("reverse", () => {
-
+    it("should return a new reversed version of the string. e.g. Hello should return elloH"() => {
+      const obj = new Reverse(str1);
+      //expect is your assertion/expectation being tested
+      expect("string" in str1).toEqual(true);
   });
 
-  describe("isPalindrome", () => {
-   
-  });
+});
 
-  describe("capitalize", () => {
-    
-  });
+//arrange
+//act
+//assert
+
+describe("isPalindrome", () => {
+
+});
+
+describe("capitalize", () => {
+
+});
 });
